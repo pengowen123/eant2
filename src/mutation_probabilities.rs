edc4b3afed7c8ee731b829e2010fa69cb82e15d9
Will Brickner <wgbrickner@gmail.com>
2022-05-05 15:43:20 -0500

    Improved mutation probabilities API and documentation

--- src/mutation_probabilities.rs
+++ src/mutation_probabilities.rs
@@ -3,7 +3,7 @@ use rand::prelude::Rng;
 use rand_distr::{Distribution, WeightedAliasIndex, WeightedError};
 use std::convert::TryFrom;
 
-/// Describes the relative probabilites of different structural mutations.
+/// Generates different structural mutations. Uses configurable relative probabilites.
 ///
 /// - Use `MutationProbabilities` to create a `MutationSampler`.
 #[derive(Clone)]
@@ -12,10 +12,10 @@ pub struct MutationSampler(WeightedAliasIndex<u16>);
 impl MutationSampler {
     /// Table of `::sample()` output values.
     const OUTPUTS: [MutationType; 4] = [
-      MutationType::AddConnection,
-      MutationType::RemoveConnection,
-      MutationType::AddNode,
-      MutationType::AddBias
+        MutationType::AddConnection,
+        MutationType::RemoveConnection,
+        MutationType::AddNode,
+        MutationType::AddBias,
     ];
 
     fn assemble(probabilities: (u16, u16, u16, u16)) -> Result<Self, WeightedError> {
@@ -32,7 +32,7 @@ impl MutationSampler {
     pub fn sample<R: Rng>(&self, rng: &mut R) -> MutationType {
         let index = self.0.sample(rng);
 
-        // soundness proof: 
+        // soundness proof:
         //   the probability list is only 4 elements long, see `::assemble`.
         //   the returned index is guaranteed to fall in [0, 3].
         unsafe { *Self::OUTPUTS.get_unchecked(index) }
@@ -40,15 +40,14 @@ impl MutationSampler {
 }
 
 impl Default for MutationSampler {
-  fn default() -> Self {
-    // safe unwrap. infallible because of default parameter choice.
-    MutationSampler::assemble((3, 8, 1, 3)).unwrap()
-  }
+    fn default() -> Self {
+        // safe unwrap. infallible because of default parameter choice.
+        MutationSampler::assemble((3, 8, 1, 3)).unwrap()
+    }
 }
 
 /// Easier to use mutation probabilities.
-/// - Input is a tuple of probabilities for each mutation.
-/// - Automatically normalized upon conversion to `MutationSampler`.
+/// - Probabilities automatically normalized upon conversion to `MutationSampler`.
 #[derive(Copy, Clone)]
 pub struct MutationProbabilities((f64, f64, f64, f64));
 
@@ -56,17 +55,19 @@ impl TryFrom<MutationProbabilities> for MutationSampler {
     type Error = WeightedError;
 
     fn try_from(p: MutationProbabilities) -> Result<Self, WeightedError> {
-        let p = p.0;
-        let sum = p.0 + p.1 + p.2 + p.3;
+        let MutationProbabilities((a, b, c, d)) = p;
 
         // map to u16s with maximum possible precision
-        let scaling = (u16::MAX as f64) / sum;
+        let scaling = {
+            let sum = a + b + c + d;
+            (u16::MAX as f64) / sum
+        };
 
         MutationSampler::assemble((
-            (p.0 * scaling) as u16,
-            (p.1 * scaling) as u16,
-            (p.2 * scaling) as u16,
-            (p.3 * scaling) as u16,
+            (a * scaling) as u16,
+            (b * scaling) as u16,
+            (c * scaling) as u16,
+            (d * scaling) as u16,
         ))
     }
 }
@@ -79,35 +80,35 @@ impl MutationProbabilities {
         MutationProbabilities((0., 0., 0., 0.))
     }
 
-    /// Sugar over `try_from`. 
-    /// Will fail if any probability is negative, all are zero, or any is `f64::NAN`.
+    /// Sugar over `try_from`.
+    /// - Will fail if any probability is negative, all are zero, or any is `f64::NAN`.
     pub fn build(self) -> Result<MutationSampler, WeightedError> {
-      MutationSampler::try_from(self)
+        MutationSampler::try_from(self)
     }
 }
 
 impl MutationProbabilities {
     /// The probability of adding a new connection between neurons.  Represented as the relative size of this number to the others.
-    pub const fn add_connection(mut self, probability: f64) -> Self {
-        self.0 .0 = probability;
-        self
+    pub const fn add_connection(self, p: f64) -> Self {
+        let Self((_, b, c, d)) = self;
+        Self((p, b, c, d))
     }
 
     /// The probability of removing an existing connection between neurons.  Represented as the relative size of this number to the others.
-    pub const fn remove_connection(mut self, probability: f64) -> Self {
-        self.0 .1 = probability;
-        self
+    pub const fn remove_connection(self, p: f64) -> Self {
+        let Self((a, _, c, d)) = self;
+        Self((a, p, c, d))
     }
 
     /// The probability of adding a new neuron.  Represented as the relative size of this number to the others.
-    pub const fn add_neuron(mut self, probability: f64) -> Self {
-        self.0 .2 = probability;
-        self
+    pub const fn add_neuron(self, p: f64) -> Self {
+        let Self((a, b, _, d)) = self;
+        Self((a, b, p, d))
     }
 
     /// The probability of adding a new bias neuron. Represented as the relative size of this number to the others.
-    pub const fn add_bias(mut self, probability: f64) -> Self {
-        self.0 .3 = probability;
-        self
+    pub const fn add_bias(self, p: f64) -> Self {
+        let Self((a, b, c, _)) = self;
+        Self((a, b, c, p))
     }
 }
